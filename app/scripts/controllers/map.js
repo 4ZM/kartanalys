5614a725db7608e04cf3c5f8e50d594936a05b1b
Anders Sundman <anders@4zm.org>
2015-01-12 23:55:36 +0100

    More data and color code by some criteria

--- app/scripts/controllers/map.js
+++ app/scripts/controllers/map.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('kartanalysApp')
-  .controller('MapCtrl', function ($scope, $http) {
+  .controller('MapCtrl', function ($scope, $http, $q) {
 
     $scope.map = {
       defaults : {
@@ -16,39 +16,57 @@ angular.module('kartanalysApp')
       }
     };
 
-    // Parse local CSV file
-    Papa.parse('/data/2014_riksdagsval_per_valdistrikt.skv', {
-      header: true,
-      delimiter: ";",
-      download: true,
-      encoding: "iso-8859-1",
-      complete: function(results) {
-        // Create a dictionary that matches gis-feature names
-        results.dict = {};
-        _(results.data).forEach(function(vd) {
-          results.dict[vd.LAN + vd.KOM + vd.VALDIST] = vd;
-        });
-      }
+    var electionData = $q(function(resolve, reject) {
+
+      // Parse local CSV file
+      Papa.parse('/data/2014_riksdagsval_per_valdistrikt.skv', {
+        header: true,
+        delimiter: ";",
+        download: true,
+        encoding: "iso-8859-1",
+        complete: function(results) {
+          // Create a dictionary that matches gis-feature names
+          results.dict = {};
+          _(results.data).forEach(function(vd) {
+            results.dict[vd.LAN + vd.KOM + vd.VALDIST] = vd;
+          });
+          resolve(results.dict);
+        }
+      });
     });
 
+
+
     function onEachFeature(feature, layer) {
-      layer.bindPopup(feature.properties.VD + ": " + feature.properties.VD_NAMN);
+      var sdProc = parseFloat(layer.feature.electionData["SD proc"].replace(',', '.'));
+      var popupTitle = feature.properties.VD + ": " + feature.properties.VD_NAMN;
+
+      layer.bindPopup(popupTitle + "<br />SD: " + sdProc + "%");
+      if (sdProc > 8.0) {
+        layer.options.fillColor = "blue";
+        layer.options.fillOpacity = 0.5;
+      }
     }
 
-    $http.get("/data/valkretsar.geojson").success(function(data, status) {
-      angular.extend($scope, {
-        geojson: {
-          data: data,
-          style: {
-            fillColor: "green",
-            weight: 1,
-            opacity: 1,
-            color: 'red',
-            fillOpacity: 0.0
-          },
-          onEachFeature: onEachFeature,
-          resetStyleOnMouseout: true
-        }
+    electionData.then(function(electionData) {
+      $http.get("/data/valkretsar.geojson").success(function(data, status) {
+        _(data.features).forEach(function(f) {
+          f.electionData = electionData[f.properties.VD];
+        });
+
+        angular.extend($scope, {
+          geojson: {
+            data: data,
+            style: {
+              weight: 1,
+              opacity: 1,
+              color: 'red',
+              fillOpacity: 0
+            },
+            onEachFeature: onEachFeature,
+            resetStyleOnMouseout: true
+          }
+        });
       });
     });
   });
